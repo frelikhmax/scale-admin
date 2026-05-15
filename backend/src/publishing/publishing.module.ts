98065264cd1380c825dc63d6a9b0e6c383c0088a
frelikhmax <frelikhmax@gmail.com>
2026-05-15 09:49:57 +0200

    TASK-033 fix publishing auth module wiring

--- backend/src/publishing/publishing.module.ts
+++ backend/src/publishing/publishing.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { CatalogValidationService } from './catalog-validation.service';
 import { PublishingController } from './publishing.controller';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [AuthModule, PrismaModule],
   controllers: [PublishingController],
   providers: [CatalogValidationService],
   exports: [CatalogValidationService],
