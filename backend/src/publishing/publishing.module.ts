820ac0a8f9b5add164b54eca8fd0f68366981100
frelikhmax <frelikhmax@gmail.com>
2026-05-15 09:47:28 +0200

    TASK-033 implement catalog validation service

--- backend/src/publishing/publishing.module.ts
+++ backend/src/publishing/publishing.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
+import { CatalogValidationService } from './catalog-validation.service';
+import { PublishingController } from './publishing.controller';
 
-@Module({})
+@Module({
+  imports: [PrismaModule],
+  controllers: [PublishingController],
+  providers: [CatalogValidationService],
+  exports: [CatalogValidationService],
+})
 export class PublishingModule {}
